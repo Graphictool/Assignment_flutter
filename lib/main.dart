344b525246e20fa64e4a4cfe32ea1cb179559117
Graphictool <wdipto@gmail.com>
2023-06-05 15:50:09 +0600

    Flutter app with three pages arranged in a tab view screen with floating action button and a centred text widget displaying

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,140 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const GeeksForGeeks());
+  runApp(MyApp());
 }
 
-class GeeksForGeeks extends StatelessWidget {
-  const GeeksForGeeks({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Assignment',
+
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+
+      ), debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+  int _currentIndex = 0;
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+    _tabController.addListener(_handleTabSelection);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  void _handleTabSelection() {
+    setState(() {
+      _currentIndex = _tabController.index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    // Material App
-    return MaterialApp(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ostad'),
+      ),
 
-      // Scaffold Widget
-        home: Scaffold(
-          appBar: AppBar(
-            // AppBar takes a Text Widget
-            // in it's title parameter
-            title: const Text('GFG'),
+      drawer: Drawer(
+
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.amber,
+              ),
+              child: Text(
+                'Menue',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 25,
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text('Page 1'),
+              onTap: () {
+                Navigator.pop(context);
+                _tabController.animateTo(0);
+              },
+            ),
+            ListTile(
+              title: Text('Page 2'),
+              onTap: () {
+                Navigator.pop(context);
+                _tabController.animateTo(1);
+              },
+            ),
+            ListTile(
+              title: Text('Page 3'),
+              onTap: () {
+                Navigator.pop(context);
+                _tabController.animateTo(2);
+              },
+            ),
+          ],
+        ),
+      ),
+
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          Center(child: Text('Page 1')),
+          Center(child: Text('Page 2')),
+          Center(child: Text('Page 3')),
+        ],
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: _currentIndex == 0 ? Colors.red : _currentIndex == 1 ? Colors.green : Colors.blue,
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+
+
+
+      bottomNavigationBar: TabBar(
+
+        controller: _tabController, labelColor: Colors.amber, unselectedLabelColor: Colors.black,
+
+        tabs: const [
+
+          Tab(
+
+            text: 'Page 1',
           ),
-          body: const Center(child: Text('Hello Dipto')),
+          Tab(
 
-        ));
+            text: 'Page 2',
 
+          ),
+          Tab(
 
+            text: 'Page 3',
 
+          ),
+        ],
+      ),
+    );
   }
 }
